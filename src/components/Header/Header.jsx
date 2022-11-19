dc15700e51446ff9747ba7dc334b5893b87d21ba
viacheslavkokoev <sqralan@ukr.net>
2022-11-20 00:44:53 +0200

    init7

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,6 @@ const Header = () => {
             <span className={'username'}>
                 { user?.username }
             </span>
-            
         </div>
     );
 };
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,21 +1,24 @@
-const tg = window.Telegram.WebApp
+const tg = window.Telegram.WebApp;
 
 export function useTelegram() {
+
     const onClose = () => {
         tg.close()
     }
 
     const onToggleButton = () => {
-        if(tg.MainButton.isVisible){
-            tg.MainButton.hide()
+        if(tg.MainButton.isVisible) {
+            tg.MainButton.hide();
         } else {
-            tg.MainButton.show()}
+            tg.MainButton.show();
+        }
     }
 
     return {
-        tg,
-        user: tg.initDataUnsafe?.user,
         onClose,
         onToggleButton,
+        tg,
+        user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
     }
 }
