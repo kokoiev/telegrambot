cb7d89415959182611e55206d9b2a76210e6a910
viacheslavkokoev <sqralan@ukr.net>
2022-11-19 19:30:54 +0200

    init7

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 function App() {
-    const {tg} = useTelegram();
+    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
