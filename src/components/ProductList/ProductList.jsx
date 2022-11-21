e87be5f1f6a52599c11b1face54201a9963ae467
viacheslavkokoev <sqralan@ukr.net>
2022-11-21 08:35:28 +0200

    init7

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('https://10.132.0.4:8000/web-data', {
+        fetch('https://134.79.96.70:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
